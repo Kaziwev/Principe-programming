17ee12693ca78f0ee582c3770875fd5f3f10403e
Nurtay Kaziyev <n_kaziyev@kbtu.kz>
2024-01-19 20:15:22 +0600

    3 com

---
+++ lab1.py
@@ -0,0 +1,53 @@
+#Python Syntax
+#print("Hello World")
+
+#if 5 > 2:
+    #print("YES")
+    
+#Python Comments
+#This is a comment
+"""
+This is a comment
+written in 
+more than just one line
+"""
+
+#Python Variables
+#carname = "Volvo"
+#x=50
+
+#x=5
+#y=10
+#print(x+y)
+
+#x=5
+#y=10
+#z=x+y
+#print(z)
+#x,y,z = "Orange", "Banana", "Cherry"
+#x=y=z="Orange"
+#def  myfunc():
+#global x
+#x="fantastic"
+
+#Python data types
+#x=5
+#print(int(x))
+#x = "Hello World"
+#print(str(x))
+#x = 20.5
+#print(float(x))
+#x = ["apple", "banana", "cherry"]
+#print(list(x))
+#x = ("apple", "banana", "cherry")
+#print(tuple(x))
+#x = {"name" : "John", "age" : 36}
+#print(dict(x))
+#x = True
+#print(bool(x))
+
+#Python numbers
+x = 5
+x = 
+float
+(x)
