2414a77c8ed67e5549f9206ff5d45538ddd3b701
Nurtay Kaziyev <n_kaziyev@kbtu.kz>
2024-01-19 21:09:52 +0600

    3 com

--- lab1.py
+++ lab1.py
@@ -49,3 +49,23 @@ more than just one line
 #Python numbers
 #x = 5
 #x = float(x)
+#x=5.5
+#x=int(x)
+#x=5
+#x=complex(x)
+
+#Python string
+#x = "Hello World"
+#print(len(x))
+#txt = "Hello World"
+#x=txt[0]
+#txt = "Hello World"
+#x=txt[2:5]
+#txt = " Hello World "
+#x=txt.strip()
+#txt = "Hello World"
+#txt=txt.upper()
+#txt = "Hello World"
+#txt=txt.lower()
+#txt = "Hello World"
+#txt=txt.replace("H", "J")
