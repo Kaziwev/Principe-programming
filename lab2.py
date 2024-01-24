8db6e02b431d30f6a9e90ea7e773a13adda16bb9
Nurtay Kaziyev <n_kaziyev@kbtu.kz>
2024-01-25 00:55:03 +0600

    2 Com

--- lab2.py
+++ lab2.py
@@ -14,31 +14,42 @@
 #False
 
 #print(10*5)
+# Output:50
 
 #print(10 /5)
+# Output:2
 
 #fruits = ["apple", "banana"]
 #if "apple" in fruits:
     #print("Yes, apple is a fruit!")
+    # Output:Yes, apple is a fruit!
     
     #if 5!= 10:
   #print("5 and 10 is not equal")
+#   Output:5 and 10 is not equal
   
-#if 5 == 10 or 4 == 4:
-  #print("At least one of the statements is true")
+# if 5 == 10 or 4 == 4:
+#   print("At least one of the statements is true")
+#   Output:At least one of the statements is true
   
-  #fruits = ["apple", "banana", "cherry"]
-#print(fruits[1])
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[1])
+# Output:banana
 
-#fruits = ["apple", "banana", "cherry"]
-#fruits[0]= "kiwi"
+# fruits = ["apple", "banana", "cherry"]
+# fruits[0]= "kiwi"
+# print(fruits)
+# Output:['kiwi', 'banana', 'cherry']
 
-#fruits = ["apple", "banana", "cherry"]
-#fruits.append("orange")
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# print(fruits)
+# Output:['apple', 'banana', 'cherry', 'orange']
 
-fruits = ["apple", "banana", "cherry"]
-fruits.insert(1,"lemon")
-print
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(1,"lemon")
+# print(fruits)
+# Output:['apple', 'lemon', 'banana', 'cherry']
 
 # fruits = ["apple", "banana", "cherry"]
 # print(fruits[-1])
