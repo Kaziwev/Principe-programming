3ff32ec81e7bb803c64498db108fbe5d6a897efc
Nurtay Kaziyev <n_kaziyev@kbtu.kz>
2024-01-25 01:47:26 +0600

    2 Com

--- lab2.py
+++ lab2.py
@@ -80,3 +80,204 @@
 #print(fruits[2:5]) 
 #Output:('cherry', 'orange', 'kiwi')
 
+# fruits = {"apple", "banana", "cherry"}
+# if "apple" in fruits:
+#   print("Yes, apple is a fruit!")
+# Output:Yes, apple is a fruit!
+
+# fruits = {"apple", "banana", "cherry"}
+# fruits.add("orange")
+# print(fruits)
+# Output:{'cherry', 'apple', 'orange', 'banana'}
+
+# fruits = {"apple", "banana", "cherry"}
+# more_fruits = ["orange", "mango", "grapes"]
+# fruits.update(more_fruits)
+# print(fruits)
+# Output:{'cherry', 'orange', 'banana', 'apple', 'mango', 'grapes'}
+
+# fruits = {"apple", "banana", "cherry"}
+# fruits.remove("banana")
+# print(fruits)
+# Output:{'apple', 'cherry'}
+
+# fruits = {"apple", "banana", "cherry"}
+# fruits.discard("banana")
+# print(fruits)
+# Output:{'apple', 'cherry'}
+
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(car.get("model"))
+# output:Mustang
+
+
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# car["year"]=2020
+# print(car)
+# Output:{'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
+
+
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# car["color"]="red"
+# print(car)
+# Output:{'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
+
+
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# car.pop("model")
+# print(car)
+# Output:{'brand': 'Ford', 'year': 1964}
+
+
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# car.clear()
+# print(car)
+# Output:{}
+
+# a = 50
+# b = 10
+# if a >b:
+#     print("Hello World")
+# Output:Hello World
+
+# a = 50
+# b = 10
+# if a !=b:
+#     print("Hello World")
+# Output:Hello World
+
+# a = 50
+# b = 10
+# if a ==b:
+#     print("Yes")
+# else:
+#     print("No")
+#     Output:No
+
+# a = 50
+# b = 10
+# if a ==b:
+#     print("1")
+# elif a >b:
+#     print("2")
+# else:
+#     print("3")
+# Output:2
+
+# if a == b and c == d:
+#   print("Hello")
+#   Output:Hello
+
+#  if a == b or c == d:
+#   print("Hello")
+#   Output:Hello
+
+# if 5 > 2:
+#  print("YES")
+#  Output:YES
+
+# a = 2
+# b = 5
+# print("YES") if a==b else print("NO")
+# Output:NO
+
+# a = 2
+# b = 50
+# c = 2
+# if a==c or b==c:
+#   print("YES")
+#   Output:YES
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+#     Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# i = 1
+# while i < 6:
+#   if i == 3:
+#     break
+# i += 1
+
+
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+# print(i)
+# Output:6
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+# Output:
+#     1
+# 2
+# 3
+# 4
+# 5
+# i is no longer less than 6
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+#     Output:
+# apple
+# banana
+# cherry
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+# print(x)
+# Output:cherry
+
+# for x in range(6):
+#     print(x)
+#     Output: 
+#         0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+# print(x)
+# Output:banana
+
+
