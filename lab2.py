f9e20e02cb5c2f8d077fb80196dd02d1706770a1
Nurtay Kaziyev <n_kaziyev@kbtu.kz>
2024-01-25 00:25:54 +0600

    2 Com

--- lab2.py
+++ lab2.py
@@ -1,7 +1,61 @@
-def myfunc():
-  global x
-  x = "fantastic"
+#print(10 > 9)
+#True
 
-myfunc()
+#print(10 == 9)
+#False
 
-print("Python is " + x)
+#print(10 < 9)
+#False
+
+#print(bool("abc"))
+#True
+
+#print(bool(0))
+#False
+
+#print(10*5)
+
+#print(10 /5)
+
+#fruits = ["apple", "banana"]
+#if "apple" in fruits:
+    #print("Yes, apple is a fruit!")
+    
+    #if 5!= 10:
+  #print("5 and 10 is not equal")
+  
+#if 5 == 10 or 4 == 4:
+  #print("At least one of the statements is true")
+  
+  #fruits = ["apple", "banana", "cherry"]
+#print(fruits[1])
+
+#fruits = ["apple", "banana", "cherry"]
+#fruits[0]= "kiwi"
+
+#fruits = ["apple", "banana", "cherry"]
+#fruits.append("orange")
+
+#fruits = ["apple", "banana", "cherry"]
+#fruits.insert(1,"lemon")
+
+#fruits = ["apple", "banana", "cherry"]
+#print(fruits[-1])
+
+#fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#print(fruits[2:5])
+
+#fruits = ["apple", "banana", "cherry"]
+#print(len(fruits))
+
+#fruits = ("apple", "banana", "cherry")
+#print(fruits[0])
+
+#fruits = ("apple", "banana", "cherry")
+#print(len(fruits))
+
+"""fruits = ("apple", "banana", "cherry")
+print(fruits[-1])"""
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5]) 
